2cbb524126eaec2b17b012e738712c911dac5631
Pedro Louro <pedrolouro93@gmail.com>
2023-03-08 20:00:38 +0000

    Solved Lab without bonus

--- src/index.js
+++ src/index.js
@@ -2,22 +2,36 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  
 
   //... your code goes here
+  const price = product.querySelector('.price span');
+  const quantity = product.querySelector('.quantity input');
+  const calcProduct = price.innerText * quantity.value;
+  const getSubTotal = product.querySelector('.subtotal span');
+  getSubTotal.innerText = calcProduct;
+  return calcProduct;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
   //... your code goes here
-
+  const products = document.getElementsByClassName('product');
+  const productsArray = [...products] // converting to an array
+  // use maop to get the return from the funtion updateSubtotal() into an array
+  const subtotalValuesArray = productsArray.map(product => updateSubtotal(product));
+  
   // ITERATION 3
   //... your code goes here
+  //reduce to calc the total
+  const totalValue = subtotalValuesArray.reduce((sum, subtotal) => sum + subtotal, 0);
+  document.querySelector('#total-value span').innerText = totalValue;
 }
 
 // ITERATION 4
